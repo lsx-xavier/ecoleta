b2b538493d7917815d4563417a969a89c684ae5a
Xavier <ls.xxavier@gmail.com>
2020-10-05 14:09:24 -0300

    Finalizando as funções vistas no NWL 01 de 2020

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,14 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import multer from 'multer';
+import { celebrate, celebrator, Joi } from 'celebrate';
+
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
@@ -14,7 +19,25 @@ routes.get('/', (request, response) => {
 
 routes.get('/items', itemsController.index );
 
-routes.post('/points', pointsController.create);
+routes.post('/points',
+upload.single('image'),
+celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatspp: Joi.number().required(),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
+    city: Joi.string().required(),
+    uf: Joi.string().required().max(2),
+    items: Joi.string().required(),
+  })
+}, {
+  abortEarly: false
+}
+),
+pointsController.create);
+
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
